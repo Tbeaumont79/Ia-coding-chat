d144ef395dcfab1349f7d797b5a65486beddcc2b
tbeaumont79 <tbeaumont79@icloud.com>
2024-11-10 13:10:16 +0100

    FEAT: binding value to input

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CustomPromptInputComponent } from './components/custom-prompt-input/custom-prompt-input.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
--- front/src/app/components/custom-prompt-input/custom-prompt-input.component.ts
+++ front/src/app/components/custom-prompt-input/custom-prompt-input.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-custom-prompt-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './custom-prompt-input.component.html',
-  styleUrl: './custom-prompt-input.component.css'
+  styleUrl: './custom-prompt-input.component.css',
 })
 export class CustomPromptInputComponent {
-
+  prompt: string = '';
+  async sendAiRequest() {
+    console.log(this.prompt);
+    const response = await fetch('http://localhost:8000/generate-text', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ prompt: this.prompt }),
+    });
+    const data = await response.json();
+    console.log(data);
+  }
 }
