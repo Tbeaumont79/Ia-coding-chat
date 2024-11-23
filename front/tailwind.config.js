2b9a06a46f9d3cc13f9e1da8158e8078164b5d85
tbeaumont79 <tbeaumont79@icloud.com>
2024-11-23 23:52:53 +0100

    Feat: update design

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
+    colors: {
+      'primary': '#211951',
+      'secondary': '#836FFF',
+      'tertiary': '#15F5BA',
+      'quaternary': '#F0F3FF',
+    }
   },
   plugins: [],
 };
